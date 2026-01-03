74ec3fb01135973a7acd34c4d2fdddc29526440f
Hrishikesh <thitehrishikesh@gmail.com>
2026-01-03 11:24:22 +0530

    installed react query

---
+++ app/providers.tsx
@@ -0,0 +1,36 @@
+// app/providers.tsx
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+
+            gcTime: 5 * 60 * 1000,
+
+            refetchOnWindowFocus: true,
+
+            refetchOnMount: true,
+
+            retry: 1,
+
+            refetchOnReconnect: true,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
